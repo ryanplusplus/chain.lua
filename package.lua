8eba430efda5985ceb30b301605bcc190fc7e48c
Ryan Hartlage <ryanplusplus@gmail.com>
2016-10-12 20:25:28 -0400

    Added lit package spec

---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+  name = 'ryanplusplus/chain',
+  version = '1.0.0',
+  description = 'Library for easily building chained calls in Lua.',
+  tags = {},
+  license = 'MIT',
+  author = {
+    name = 'Ryan Hartlage'
+  },
+  homepage = 'https://github.com/ryanplusplus/chain.lua',
+  files = {
+    'src/*.lua'
+  }
+}
